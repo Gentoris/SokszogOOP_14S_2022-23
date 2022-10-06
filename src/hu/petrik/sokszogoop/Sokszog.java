ae34c2aaf51b3c8a5dbc47569979edf3f651896f
Deli Gabor <email@gmail.com>
2022-10-06 16:05:51 +0200

    Téglalap osztály

--- src/hu/petrik/sokszogoop/Sokszog.java
+++ src/hu/petrik/sokszogoop/Sokszog.java
@@ -2,4 +2,12 @@ package hu.petrik.sokszogoop;
 
 public class Sokszog {
     private double a;
+
+    public Sokszog(double a){
+        this.a = a;
+    }
+
+    public double getA(){
+        return a;
+    }
 }
